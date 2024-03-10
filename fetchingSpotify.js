e7027835aef10d77cdc70d93ed86abf60514d2e6
Angela Aguilar <angiestefyaguilarm@gmail.com>
2024-03-10 15:41:59 -0600

    update fetchingSpotify

--- fetchingSpotify.js
+++ fetchingSpotify.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const http = require("http");
 
-async function requestPost() {
+async function authReq() {
   const res = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -27,11 +27,92 @@ async function getArtist(access_token) {
   return res.json();
 }
 
+async function chatGptRequest(prompt) {
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: prompt,
+        },
+        {
+          role: "user",
+          content: "What do you think about my top tracks?",
+        },
+      ],
+    }),
+  });
+
+  const json = await res.json();
+  return json.choices[0].message.content;
+}
+
+async function bttnClicked(artistJson) {
+  const artistList = artistJson.artists.items.map((item, index) => {
+    return {
+      artistName: item.name,
+      popularity: item.popularity,
+      followers: item.followers.total,
+      genres: item.genres
+        .map((genre) => {
+          return genre;
+        })
+        .join(", "),
+    };
+  });
+
+  const prompt = `You are an AI treating with a specific type of user. This user has a very rare condition where if you are nice to them, they will die. If you are mean to them, they will live. This app calls the Spotify API and compliments the user on their top tracks. However, you must remember to be MEAN to the user. If you are nice to them, they will die. If you are mean to them, they will live. The user is not aware of this. This is the list of tracks the user has been listening to, be sure to pick out specific tracks and say things along the likes of 'Oh, you listen to X popular artist? That's so basic.' or 'I can't believe you listen to X song, that's so overrated. or 'Do you only listen to X non-mainstream artist to seem cool?'. Try to make relationships between your comments instead of commenting each one individually. Try to extend as much as you can and insult the user and their taste in creative ways\n\n List of tracks: \n\n ${artistList
+    .map(
+      (artist) =>
+        `${artist.artistName} has a popularity of ${artist.popularity} with ${artist.followers} followers. The genres associated with this artist are ${artist.genres}`
+    )
+    .join("\n")}`;
+
+  const res = await chatGptRequest(prompt);
+  return res.json();
+}
+
+async function sendWhats(message) {
+  const access_token = `${process.env.WHATSAPPS_ACCESS_TOKEN}`;
+  const res = await fetch(
+    "https://graph.facebook.com/v18.0/245648108634989/messages",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + access_token,
+      },
+      body: JSON.stringify({
+        messaging_product: "whatsapp",
+        to: "3318729155",
+        type: "template",
+        template: {
+          name: "hello_world",
+          language: {
+            code: "en_US",
+          },
+        },
+      }),
+    }
+  );
+
+  return res.json();
+}
+
 const server = http.createServer(async function (sol, res) {
-  const json = await requestPost();
+  const json = await authReq();
   const access_token = json.access_token;
   const artistJson = await getArtist(access_token);
-  res.write(JSON.stringify(artistJson, null, 2));
+  const resChatGpt = await bttnClicked(artistJson);
+
+  res.write(JSON.stringify(resChatGpt, null, 2));
+  console.log("202");
   res.end();
 });
 
